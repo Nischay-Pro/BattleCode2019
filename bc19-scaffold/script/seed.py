04df72728825e74c194e6fbf38412e6f5867d0a1
Nischay Ram Mamidi <NischayPro@gmail.com>
2019-01-13 19:09:17 +0530

    seed now has verbose

--- bc19-scaffold/script/seed.py
+++ bc19-scaffold/script/seed.py
@@ -12,19 +12,25 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--test", const=str, nargs="?")
+    parser.add_argument("-l", "--less", const=str, nargs="?")
     args = parser.parse_args()
     redpath = str(args.test)
+    less = int(args.less)
     bluepath = redpath
-
-    
+    lessmoded = False
+    if less == 1:
+        lessmoded = True
+    else:
+        lessmoded = False
 
     clearScreen()
     cprint("Starting up", "red")
     stats = {
         "turn":"Preparing...",
         "bot": os.path.basename(os.path.normpath(redpath)),
+        "args": "Waiting",
         "seed":{
-
+            1:"Checking..."
         }
     }
     checktime = time.time()
@@ -32,69 +38,101 @@ def main():
     os.makedirs("seed_logs")
     for seed in range(1,1001):
         stats["turn"] = "Preparing..."
-        pretty_print(stats, seed)
+        pretty_print(stats, seed, lessmode=lessmoded)
         process = subprocess.Popen(["bc19run", "-b", bluepath, "-r", redpath, "-d", "false", "-s", str(seed)], stdout=subprocess.PIPE)
+        # lprint("Testing on Seed %s" % seed, lessmoded)
+        stats["args"] = " ".join(process.args)
         verbose = []
         counter = 10
         for line in iter(process.stdout.readline, b''):
             data = line.decode(sys.stdout.encoding)
-            verbose.append(data)
+            data = data.encode('utf-8').decode('utf-8')
+            data = str(data)
             if "Script Helper Turn" in data:
                 currtime = time.time()
                 if currtime - checktime > 2:
-                    data = data.split("\n")[0]
-                    data = data.split("@")
-                    stats["turn"] = data[1]
-                    pretty_print(stats, seed)
+                    data2 = data.split("\n")[0]
+                    data2 = data.split("@")
+                    stats["turn"] = data2[1].strip()
+                    pretty_print(stats, seed, lessmode=lessmoded)
                     checktime = time.time()
+            verbose.append(data)
             if counter < 10:
                 counter -= 1
                 if counter <= 0:
+                    stats["seed"][seed] = "Failed"
                     with open('seed_logs/seed_%s.txt' % seed, mode='wt', encoding='utf-8') as myfile:
                         myfile.write('\n'.join(verbose))
                     process.terminate()
                     break
             if "vm.js" in data:
                 stats["seed"][seed] = "Failed"
+                lprint("Seed %s Failed. Script Error" % seed, lessmoded)
                 counter -= 1
-            if "Game over, blue won " in data:
-                if stats["seed"][seed] == "Time Failed":
-                    stats["seed"][seed] = "Passed (Time Failed)"
-                break
-            if "Robot is frozen due to clock overdrawn by" in data:
+            if "Robot is frozen due" in data:
                 stats["seed"][seed] = "Time Failed"
-            elif "Game over, red won " in data:
+            if "blue won" in data:
                 if stats["seed"][seed] == "Time Failed":
                     stats["seed"][seed] = "Passed (Time Failed)"
+                lprint("Seed %s Passed (Time Failed)" % seed, lessmoded)
+                pretty_print(stats, seed, lessmode=lessmoded)
+                process.terminate()
                 break
-    pretty_print(stats, 1000, done=True)
+            if "red won" in data:
+                if stats["seed"][seed] == "Time Failed":
+                    stats["seed"][seed] = "Passed (Time Failed)"
+                lprint("Seed %s Passed (Time Failed)" % seed, lessmoded)
+                pretty_print(stats, seed, lessmode=lessmoded)
+                process.terminate()
+                break
+            if "failed to initialize" in data:
+                stats["seed"][seed] = data.strip()
+                pretty_print(stats, seed, lessmode=lessmoded)
+                lprint("Seed %s Failed. Initialization Error" % seed, lessmoded)
+            with open('seed_logs/seed_%s.txt' % seed, mode='wt', encoding='utf-8') as myfile:
+                myfile.write('\n'.join(verbose))
+    pretty_print(stats, 1000, done=True, lessmode=lessmoded)
+    lprint("Done", lessmoded)
 
-    
-def pretty_print(stats, seed, done=False):
-    clearScreen()
-    cprint("******************************", "magenta")
-    if done == False:
-        cprint("Running Seed Testing!", "red", attrs=['bold'])
-    else:
-        cprint("Seed Testing Finished!", "red", attrs=['bold'])
-    cprint("Bot: %s" % stats["bot"], "green")
-    cprint("Seed: %s / %s" % (seed, 1000), "white")
-    cprint("Turn Status:")
-    turn = stats["turn"]
-    if turn != "Preparing...":
-        cprint("\t Turn\t: %s / 999" % (turn), "white")
-    else:
-        cprint("\t Turn\t: %s" % (turn))
-    cprint("Seed Status:", "cyan")
-    for itm in stats["seed"].keys():
-        status = stats["seed"][itm]
-        color = "red"
-        if "Passed" in status:
-            color = "blue"
-        if "Time Failed" in status:
-            color = "yellow"
-        cprint("\t Seed %s\t: %s" % (itm, status), color)
-    cprint("******************************", "magenta")
+
+def lprint(whattoprint, lessmode):
+    if lessmode:
+        print(whattoprint)
+
+def pretty_print(stats, seed, done=False, lessmode=False):
+    if lessmode == False:
+        try:
+            clearScreen()
+            cprint("******************************", "magenta")
+            if done == False:
+                cprint("Running Seed Testing!", "red", attrs=['bold'])
+            else:
+                cprint("Seed Testing Finished!", "red", attrs=['bold'])
+            cprint("Bot: %s" % stats["bot"], "green")
+            cprint("Argument: %s" % stats["args"], "green")
+            cprint("Seed: %s / %s" % (seed, 1000), "white")
+            cprint("Turn Status:")
+            turn = stats["turn"]
+            if turn != "Preparing...":
+                cprint("\t Turn\t: %s / 999" % (turn), "white")
+            else:
+                cprint("\t Turn\t: %s" % (turn))
+            cprint("Seed Status:", "cyan")
+            for itm in stats["seed"].keys():
+                status = stats["seed"][itm]
+                color = "red"
+                if "Passed" in status:
+                    color = "blue"
+                if "Time Failed" in status:
+                    color = "yellow"
+                if "Checking..." in status:
+                    color = "blue"
+                if "failed to initialize" in status:
+                    color = "red"
+                cprint("\t Seed %s\t: %s" % (itm, status), color)
+            cprint("******************************", "magenta")
+        except KeyError:
+            print(stats)
 
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
