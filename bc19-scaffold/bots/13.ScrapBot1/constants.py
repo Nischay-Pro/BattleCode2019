1ed3a46a97d1f7c27d7fe6bf3d3c298d81a91a7f
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-14 10:58:10 +0530

    The stupidity of man knwos no bounds aka  we could store stuff in variables

---
+++ bc19-scaffold/bots/13.ScrapBot1/constants.py
@@ -0,0 +1,65 @@
+from battlecode import SPECS
+
+pathfinding_heuristic_multiplier = 1.1
+
+pilgrim_will_scavenge_closeby_mines_before_turns = 10
+pilgrim_will_scavenge_closeby_mines = 50
+chokepoint_modifier = .4
+karbonite_modifier = .05
+fuel_modifier = .05
+
+directions = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+
+enemy_unit_priority_for_prophet = [ 
+    2, # Castle
+    2, # Church
+    3, # Pilgrim
+    4, # Crusader
+    4, # Prophet
+    10, # Preacher
+]
+
+unit_castle = SPECS['CASTLE']
+unit_church = SPECS['CHURCH']
+unit_crusader = SPECS['CRUSADER']
+unit_pilgrim = SPECS['PILGRIM']
+unit_preacher = SPECS['PREACHER']
+unit_prophet = SPECS['PROPHET']
+
+# Attack Damage
+crusader_attack_damage = SPECS['UNITS'][SPECS["CRUSADER"]]['ATTACK_DAMAGE']
+prophet_attack_damage = SPECS['UNITS'][SPECS["PROPHET"]]['ATTACK_DAMAGE']
+preacher_attack_damage = SPECS['UNITS'][SPECS["PREACHER"]]['ATTACK_DAMAGE']
+
+# Attack Range
+crusader_min_attack_range = SPECS['UNITS'][SPECS["CRUSADER"]]['ATTACK_RADIUS'][0]
+crusader_max_attack_range = SPECS['UNITS'][SPECS["CRUSADER"]]['ATTACK_RADIUS'][1]
+prophet_min_attack_range = SPECS['UNITS'][SPECS["PROPHET"]]['ATTACK_RADIUS'][0]
+prophet_max_attack_range = SPECS['UNITS'][SPECS["PROPHET"]]['ATTACK_RADIUS'][1]
+preacher_min_attack_range = SPECS['UNITS'][SPECS["PREACHER"]]['ATTACK_RADIUS'][0]
+preacher_max_attack_range = SPECS['UNITS'][SPECS["PREACHER"]]['ATTACK_RADIUS'][1]
+
+# Initial Health
+crusader_max_health = SPECS['UNITS'][SPECS["CRUSADER"]]['STARTING_HP']
+prophet_max_health = SPECS['UNITS'][SPECS["PROPHET"]]['STARTING_HP']
+preacher_max_health = SPECS['UNITS'][SPECS["PREACHER"]]['STARTING_HP']
+
+pilgrim_will_scavenge_closeby_mines_after_turns = 50
+
+
+# SPEC API
+# public int CONSTRUCTION_KARBONITE;
+# public int CONSTRUCTION_FUEL;
+# public int KARBONITE_CAPACITY;
+# public int FUEL_CAPACITY;
+# public int SPEED;
+# public int FUEL_PER_MOVE;
+# public int STARTING_HP;
+# public int VISION_RADIUS;
+# public int ATTACK_DAMAGE;
+# public int[] ATTACK_RADIUS;
+# public int ATTACK_FUEL_COST;
+# public int DAMAGE_SPREAD;
+
+def get_required_constant():
+    None
---
+++ bc19-scaffold/bots/13.ScrapBot1/preachers.py
@@ -0,0 +1,23 @@
+import utility
+import combat_module
+
+def preacher(robot):
+    return preacher_move(robot)
+
+def preacher_move(robot):
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    passable_map = robot.get_passable_map()
+    occupied_map = robot.get_visible_robot_map()
+    directions = utility.random_cells_around()
+
+    preacher_attack_aggr_mode = combat_module.give_military_command(robot)
+    if preacher_attack_aggr_mode != None:
+        return preacher_attack_aggr_mode
+
+    if utility.fuel_less_check(robot):
+        return None
+
+    for direction in directions:
+        if (not utility.is_cell_occupied(occupied_map, pos_x + direction[1],  pos_y + direction[0])) and passable_map[pos_y + direction[0]][pos_x + direction[1]] == 1:
+            return robot.move(direction[1], direction[0])
