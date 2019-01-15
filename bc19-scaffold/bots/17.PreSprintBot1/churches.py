e89ecbb86b89c4771f1f4bae40f688c8bbbd8662
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-15 17:58:33 +0530

    Backup bot, work now progresses to presprint bot. Sprint submission  in 12 hours

---
+++ bc19-scaffold/bots/17.PreSprintBot1/churches.py
@@ -0,0 +1,15 @@
+import utility
+import constants
+import production_module
+
+# Add code for locked castles
+
+def church(robot):
+    if robot.step < 2:
+        # self.log("Building a crusader at " + str(self.me['x']+1) + ", " + str(self.me['y']+1))
+        return production_module.default_production_order(robot)
+    # elif robot.step > 500 and robot.karbonite > 100 and robot.fuel > 200:
+    #     return castle_build(robot, SPECS['PILGRIM'])
+    else:
+        None
+        # self.log("Castle health: " + self.me['health'])
