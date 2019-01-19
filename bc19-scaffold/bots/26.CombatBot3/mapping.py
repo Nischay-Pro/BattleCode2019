07dc50a82ef4241e536f5175a8dd363ed015778b
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-19 18:40:09 +0530

    The regular backup update

---
+++ bc19-scaffold/bots/26.CombatBot3/mapping.py
@@ -0,0 +1,194 @@
+import constants
+
+def get_nearby_map(x, y, given_map, grid_radius = 2):
+    sub_side = grid_radius * 2 + 1
+    sub_map = []
+
+    for i in range(sub_side):
+        for j in range(sub_side):
+            try:
+                sub_map.append(given_map[y-grid_radius+i][x-grid_radius+j] == True)
+            except:
+                sub_map.append(False)
+
+    return sub_map
+
+def get_map_ratio(x, y, given_map, grid_radius = 2):
+    nearby = get_nearby_map(x, y, given_map, grid_radius)
+    full = 0
+
+    for cell in nearby:
+        if cell == True:
+            full += 1
+
+    return full/((grid_radius * 2 + 1)**2)
+
+def analyze_map(given_map, grid_radius = 2):
+    sub_side = grid_radius * 2 + 1
+    results = []
+
+    y = grid_radius + 1
+    while y < len(given_map):
+        x = grid_radius + 1
+        while x < len(given_map):
+            results.append((x, y, get_map_ratio(x, y, given_map, grid_radius)))
+            x += sub_side
+        y += sub_side
+
+    return results
+
+def find_symmetrical_point(robot, pos_x, pos_y, is_hoz_symmetry):
+    map_length = len(robot.get_passable_map())
+    if is_hoz_symmetry == 0:
+        return (pos_x, map_length - 1 - pos_y)
+    else:
+        return (map_length - 1 - pos_x, pos_y)
+
+def return_map_symmetry(robot):
+    passable_map = robot.get_passable_map()
+    if check_hoz_symmetry(passable_map):
+        # robot.log("Is horizontal")
+        robot.map_symmetry = 0
+    else:
+        # robot.log("Is vertical")
+        robot.map_symmetry = 1
+
+def check_hoz_symmetry(given_map):
+    start = 0
+    end = len(given_map) - 1
+
+    while start < end:
+        for i in range(len(given_map[start])):
+            if given_map[start][i] != given_map[end][i]:
+                return False
+        start += 1
+        end -= 1
+
+    return True
+
+def find_chokepoints(robot, grid_radius = 2):
+    given_map = robot.get_passable_map()
+    sub_side = grid_radius * 2 + 1
+    results = []
+
+    y = grid_radius + 1
+    while y < len(given_map):
+        x = grid_radius + 1
+        while x < len(given_map):
+            ratio = get_map_ratio(x, y, given_map, grid_radius)
+            if ratio < constants.chokepoint_modifier:
+                results.append((x, y, ratio))
+            x += sub_side
+        y += sub_side
+    return results
+
+def find_karbonite_rich(robot, grid_radius = 2):
+    given_map = robot.karbonite_map
+    sub_side = grid_radius * 2 + 1
+    results = []
+
+    y = grid_radius + 1
+    while y < len(given_map):
+        x = grid_radius + 1
+        while x < len(given_map):
+            ratio = get_map_ratio(x, y, given_map, grid_radius)
+            if ratio > constants.karbonite_modifier:
+                results.append((x, y, ratio))
+            x += sub_side
+        y += sub_side
+
+    return results
+
+def find_fuel_rich(robot, grid_radius = 2):
+    given_map = robot.fuel_map
+    sub_side = grid_radius * 2 + 1
+    results = []
+
+    y = grid_radius + 1
+    while y < len(given_map):
+        x = grid_radius + 1
+        while x < len(given_map):
+            ratio = get_map_ratio(x, y, given_map, grid_radius)
+            if ratio > constants.fuel_modifier:
+                results.append((x, y, ratio))
+            x += sub_side
+        y += sub_side
+
+    return results
+
+def find_resource_rich(robot, grid_radius = 2):
+    fuel_map = robot.fuel_map
+    karbonite_map = robot.karbonite_map
+    sub_side = grid_radius * 2 + 1
+    results = []
+
+    y = grid_radius + 1
+    while y < len(fuel_map):
+        x = grid_radius + 1
+        while x < len(fuel_map):
+            ratio = get_map_ratio(x, y, fuel_map, grid_radius) + get_map_ratio(x, y, karbonite_map, grid_radius)
+            if ratio > constants.fuel_modifier:
+                results.append((x, y, ratio))
+            x += sub_side
+        y += sub_side
+
+    return results
+
+def get_friendly_influence(robot):
+    visible_map = robot.get_visible_robot_map()
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    side = len(visible_map)
+
+    visible_area = 0
+    visible_friendlies = 0
+
+    for i in range(side):
+        for j in range(side):
+            if visible_map[i][j] > -1:
+                visible_area += 1
+                if visible_map[i][j] > 0:
+                    if robot.get_robot(visible_map[i][j]).team == robot.me.team:
+                        visible_friendlies += 1
+
+    return visible_friendlies / visible_area
+
+def get_enemy_influence(robot):
+    visible_map = robot.get_visible_robot_map()
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    side = len(visible_map)
+
+    visible_area = 0
+    visible_enemies = 0
+
+    for i in range(side):
+        for j in range(side):
+            if visible_map[i][j] > -1:
+                visible_area += 1
+                if visible_map[i][j] > 0:
+                    if robot.get_robot(visible_map[i][j]).team != robot.me.team:
+                        visible_enemies += 1
+
+    return visible_enemies / visible_area
+
+def get_tension(robot):
+    visible_map = robot.get_visible_robot_map()
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    side = len(visible_map)
+
+    visible_area = 0
+    visible_friendlies = 0
+
+    for i in range(side):
+        for j in range(side):
+            if visible_map[i][j] > -1:
+                visible_area += 1
+                if visible_map[i][j] > 0:
+                    if robot.get_robot(visible_map[i][j]).team == robot.me.team:
+                        visible_friendlies += 1
+                    else:
+                        visible_friendlies -= 1
+
+    return visible_friendlies / visible_area
---
+++ bc19-scaffold/bots/26.CombatBot3/production_module.py
@@ -0,0 +1,113 @@
+import constants
+import utility
+import check
+import vision
+import castles_utility
+
+
+def _build_manager_castle(robot):
+    castle_count = 0
+    church_count = 0
+    crusader_count = 0
+    pilgrim_count = 0
+    preacher_count = 0
+    prophet_count = 0
+    friendly_units, enemy_units = castles_utility.castle_all_friendly_units(robot)
+    total_karbonite = vision.all_karbonite(robot)
+    total_fuel = vision.all_fuel(robot)
+
+    # TODO - Build combat units, link with production modules?
+    castles_utility._is_castle_under_attack(robot, enemy_units)
+
+    # robot.log(mapping.analyze_map(robot.get_passable_map()))
+
+    for f_unit in friendly_units:
+        if f_unit.castle_talk == constants.unit_castle:
+            castle_count+=1
+        elif f_unit.castle_talk == constants.unit_church:
+            church_count+=1
+        elif f_unit.castle_talk == constants.unit_crusader:
+            crusader_count+=1
+        elif f_unit.castle_talk == constants.unit_pilgrim:
+            pilgrim_count+=1
+        elif f_unit.castle_talk == constants.unit_preacher:
+            preacher_count+=1
+        elif f_unit.castle_talk == constants.unit_prophet:
+            prophet_count+=1
+
+    """ Building units -
+        Start with 2 pilgrims per castle (as long as karbonite after building remains above 50).
+        If sufficient resources(>100 karb, >200 fuel), build, in order -
+            1 crusader per 3 pilgrims
+            1 preacher per 2 crusaders (per 6 pilgrims)
+            1 prophet per 3 crusaders (per 9 pilgrims)
+            1 prophet per 2 resources on map
+    """
+
+    # robot.log(str(robot.me.signal))
+
+    if robot.castle_under_attack > 0:
+        None
+        #TODO Broadcast with Co-ordinates to send troops. Range set to max of map.
+    else:
+        # Peaceful Conditions
+        if prophet_count < 2 and robot.karbonite >= 25 and robot.step < 10:
+            robot.signal(1, 2)
+            return castles_utility._castle_build(robot, constants.unit_prophet)
+        elif robot.karbonite >= 15 and robot.fuel > 100 and pilgrim_count < (total_fuel + total_karbonite) * .35 and robot.step < 60:
+            if prophet_count < pilgrim_count/2:
+                robot.signal(1, 2)
+                return castles_utility._castle_build(robot, constants.unit_prophet)
+            else:
+                robot.pilgrim_build_number += 1
+                temp_store = castles_utility._castle_assign_mine_or_scout(robot)
+                if temp_store != 0:
+                    robot.signal(temp_store, 2)
+                else:
+                    robot.signal(65534, 2)
+                return castles_utility._castle_build(robot,constants.unit_pilgrim)
+        elif robot.karbonite > 100 and robot.fuel > 200:
+            #  if (crusader_count * 3) < pilgrim_count:
+                #  # robot.signal(robot.me.signal + 1, 2)
+                #  return castle_build(robot,constants.unit_crusader)
+            # elif (preacher_count * 2) < crusader_count:
+            #     # robot.signal(robot.me.signal + 1, 2)
+            #     return castle_build(robot, constants.unit_preacher)
+            if prophet_count < pilgrim_count and robot.step > 60:
+                robot.signal(1, 2)
+                return castles_utility._castle_build(robot, constants.unit_prophet)
+            if pilgrim_count < (total_fuel + total_karbonite) * .55:
+                robot.pilgrim_build_number += 1
+                temp_store = castles_utility._castle_assign_mine_or_scout(robot)
+                if temp_store != 0:
+                    robot.signal(temp_store, 2)
+                else:
+                    robot.signal(65534, 2)
+                return castles_utility._castle_build(robot,constants.unit_pilgrim)
+            elif robot.step > 300 and robot.karbonite > 600 and robot.fuel > 600:
+                robot.signal(1, 2)
+                return castles_utility._castle_build(robot, constants.unit_prophet)
+            elif robot.step > 500 and robot.karbonite > 300 and robot.fuel > 300:
+                robot.signal(1, 2)
+                return castles_utility._castle_build(robot, constants.unit_prophet)
+
+def _build_manager_church(robot):
+    unit_type = constants.unit_prophet
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    passable_map, occupied_map, karb_map, fuel_map = utility.get_all_maps(robot)
+    directions = utility.random_cells_around()
+    for direction in directions:
+        if (not utility.is_cell_occupied(occupied_map, pos_x + direction[1],  pos_y + direction[0])) and passable_map[pos_y + direction[0]][pos_x + direction[1]] == 1:
+            # TRAVIS BUILD CHECK 4
+            return check.build_check(robot, unit_type, direction[1], direction[0], 4)
+    # robot.log("No space to build units anymore for churches")
+    return None
+
+
+def default_production_order(robot):
+    unit_type = robot.me.unit
+    if unit_type == constants.unit_church:
+        return _build_manager_church(robot)
+    if unit_type == constants.unit_castle:
+        return _build_manager_castle(robot)
